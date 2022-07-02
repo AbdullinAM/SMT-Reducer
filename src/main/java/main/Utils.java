53fe4e090ee438e94692b801413586fb97910d15
vladislav <1>
2022-07-02 16:23:09 +0300

    refactor is done

---
+++ src/main/java/main/Utils.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class Utils {
+    private Utils() {
+
+    }
+
+    private static final String LABEL = "label=\"";
+
+    public static String getLabel(String line) {
+        int labelIndex = line.indexOf(LABEL);
+        int lastQuoteIndex = line.lastIndexOf("\"");
+        String contradiction = line.substring(labelIndex + LABEL.length(), lastQuoteIndex);
+        int indexOfFalse = contradiction.lastIndexOf("false");
+        if (indexOfFalse == -1) {
+            return contradiction;
+        }
+        String str = "(=";
+        int startIndex = contradiction.indexOf(str) + str.length();
+        return contradiction.substring(startIndex, indexOfFalse - 1).trim();
+    }
+
+    public static List<List<Node>> split(List<Node> s, int n) {
+        List<List<Node>> subsets = new ArrayList<>();
+        int position = 0;
+        for (int i = 0; i < n; i++) {
+            List<Node> subset = s.subList(position, position + (s.size() - position) / (n - i));
+            subsets.add(subset);
+            position += subset.size();
+        }
+        return subsets;
+    }
+}
